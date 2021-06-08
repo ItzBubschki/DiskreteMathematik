6f7bd319102eedbfcbd63903e5bb98a1cae1a033
LaurinNiemeyer <laurin@livelyapps.com>
2021-06-08 18:00:21 +0200

    fixed the power mod

--- power_mod.py
+++ power_mod.py
@@ -17,6 +17,25 @@ def phi_func(x):
         n = [y for y in range(1,x) if is_coprime(x,y)]
         return len(n)
 
+def calculate_list(base, exponent, modulo):
+    recieved_results = []
+    repeting_num = -1
+    for i in range(exponent):
+        result = (base**i)%modulo
+        if not recieved_results.__contains__(result):
+            recieved_results.append(result)
+            print(f"{base}^{i} = {base**i} = {result}.")
+        else:
+            print(f"{base}^{i} = {result}")
+            repeting_num = i - recieved_results.index(result)
+            break
+    if repeting_num == -1 or repeting_num == 1:
+        print(f"{base}^{exponent} mod {modulo} = {(base**exponent)%modulo}")
+    else:
+        expo_mod = repeting_num-1
+        new_expo = exponent%expo_mod
+        print(f"{base}^{exponent} mod {expo_mod} = {base}^{new_expo} = {base**new_expo}%{modulo} = {(base**new_expo)%modulo}.")
+
 if gcd(base, modulo) == 1:
     phi = phi_func(modulo)
     print(f"Phi is {phi} and the gcd is 1.")
@@ -30,21 +49,7 @@ if gcd(base, modulo) == 1:
     for element in multi_exponets.split():
         multi_exponents_list.append(int(element))
     
-    recieved_results = []
-    for i in range(multi_exponents_list[0]):
-        result = (new_base**i)%modulo
-        if not recieved_results.__contains__(result):
-            recieved_results.append(result)
-            print(f"{new_base}^{i} = {new_base**i} = {result}.")
-        else:
-            print(f"{new_base}^{i} = {result}")
-            break
-    overview_text = ""
-    for expo in multi_exponents_list:
-        overview_text += f"{new_base}^{expo} "
-        if not multi_exponents_list[len(multi_exponents_list)-1] == expo:
-            overview_text += "* "
-    print(overview_text)
+    calculate_list(new_base, multi_exponents_list[0], modulo)
 
     result_text = ""
     for expo in multi_exponents_list:
@@ -54,19 +59,5 @@ if gcd(base, modulo) == 1:
     print(result_text)
     print(f"Result for {base}^{exponent}%{modulo} = {new_base}^{new_exponent}%{modulo} = {(new_base**new_exponent)%modulo}")
 else:
-    print("The gcd is greater then 1, so the euler algorithm doesn't work.")
-    recieved_results = []
-    repeting_num = -1
-    for i in range(exponent):
-        result = (base**i)%modulo
-        if not recieved_results.__contains__(result):
-            recieved_results.append(result)
-            print(f"{base}^{i} = {base**i} = {result}.")
-        else:
-            print(f"{base}^{i} = {result}")
-            repeting_num = i
-            break
-    if not repeting_num == -1:
-        expo_mod = repeting_num-1
-        new_expo = exponent%expo_mod
-        print(f"{base}^{exponent} mod {expo_mod} = {base}^{new_expo} = {base**new_expo}%{modulo} = {(base**new_expo)%modulo}.")
+    print(f"The gcd is greater then 1 ({gcd(base, modulo)}), so the euler algorithm doesn't work.")
+    calculate_list(base, exponent, modulo)
